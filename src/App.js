19567a2f33ba777e4adb2b252caf1f2bd2317e26
Dorian PELLETIER <pelletier.dorian@urbasolar.com>
2022-02-24 17:37:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// TODO : Ajouter un backend Node.js / Express et une BDD Mongo Atlas afin de rendre les données persistantes
+
+// Permet d'afficher les Todo
+function Todo({ todo, index, completeTodo, deleteTodo }) {
+	return (
+		<div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+			{todo.text}
+			<div>
+				<button onClick={() => completeTodo(index)}>Complete</button>
+				<button onClick={() => deleteTodo(index)}>X</button>
+			</div>
+		</div>
+	);
+}
+
+// Formulaire permettant d'ajouter des ToDo
+function TodoForm({ addTodo }) {
+	const [value, setValue] = React.useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!value) return;
+		addTodo(value);
+		setValue("");
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type="text" className="input" value={value} onChange={(e) => setValue(e.target.value)} />
+		</form>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = React.useState([
+		{
+			text: "Learn about React",
+			isCompleted: false,
+		},
+		{
+			text: "Meet friend for lunch",
+			isCompleted: false,
+		},
+		{
+			text: "Build really cool todo app",
+			isCompleted: false,
+		},
+	]);
+
+	const addTodo = (text) => {
+		const newTodos = [...todos, { text }];
+		setTodos(newTodos);
+	};
+
+	const completeTodo = (index) => {
+		const newTodos = [...todos];
+		newTodos[index].isCompleted = !newTodos[index].isCompleted;
+		setTodos(newTodos);
+	};
+
+	const deleteTodo = (index) => {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+
+	return (
+		<div className="app">
+			<div className="todo-list">
+				{todos.map((todo, index) => (
+					<Todo key={index} index={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} />
+				))}
+				<TodoForm addTodo={addTodo} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
